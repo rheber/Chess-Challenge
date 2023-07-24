dfe544466c0f5913abe1ab8ab51df53554ded770
rheber <rheber@rediffmail.com>
2023-07-24 21:57:28 +1000

    move with default

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -2,16 +2,34 @@
 using ChessChallenge.API;
 
 public class MyBot : IChessBot {
-    private int turnCounter;
+    int turnCounter;
 
     public MyBot() {
         turnCounter = 1;
     }
 
+    Move def(Board board) {
+        Move[] moves = board.GetLegalMoves();
+        return moves[0];
+    }
+
+    Move withDefault(Move move, Board board) {
+        foreach (Move m in board.GetLegalMoves()) {
+            if (move.StartSquare == m.StartSquare && move.TargetSquare == m.TargetSquare) {
+                return move;
+            }
+        }
+        return def(board);
+    }
+
     public Move Think(Board board, Timer timer) {
         try {
-            Move[] moves = board.GetLegalMoves();
-            return moves[0];
+            if (turnCounter == 1) {
+                if (board.IsWhiteToMove) {
+                    return withDefault(new Move("g1f3", board), board);
+                }
+            }
+            return def(board);
         } finally {
             //Console.WriteLine(turnCounter);
             turnCounter += 1;
