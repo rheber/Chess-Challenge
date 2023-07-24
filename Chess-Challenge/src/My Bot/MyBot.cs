2d889b61c62e0ecc7fa38f8a30d1203a4a702148
rheber <rheber@rediffmail.com>
2023-07-24 21:36:09 +1000

    turn counter

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,10 +1,20 @@
-﻿using ChessChallenge.API;
+﻿using System;
+using ChessChallenge.API;
 
-public class MyBot : IChessBot
-{
-    public Move Think(Board board, Timer timer)
-    {
-        Move[] moves = board.GetLegalMoves();
-        return moves[0];
+public class MyBot : IChessBot {
+    private int turnCounter;
+
+    public MyBot() {
+        turnCounter = 1;
+    }
+
+    public Move Think(Board board, Timer timer) {
+        try {
+            Move[] moves = board.GetLegalMoves();
+            return moves[0];
+        } finally {
+            //Console.WriteLine(turnCounter);
+            turnCounter += 1;
+        }
     }
 }
