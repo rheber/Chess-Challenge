3e0266ea4803b32dc7ca78344538ba8c9e46b523
rheber <rheber@rediffmail.com>
2023-07-25 21:16:43 +1000

    Most forcing moves (cap 235, vs evilbot 0W/20D/10L)

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,4 +1,5 @@
 ﻿using ChessChallenge.API;
+using System.Collections.Generic;
 
 public class MyBot : IChessBot {
     int turnsElapsed;
@@ -9,7 +10,7 @@ public class MyBot : IChessBot {
 
     // Default move.
     Move def(Board board) {
-        return board.GetLegalMoves()[0];
+        return mostForcingMoves(board)[0];
     }
 
     // Try to make the given move, make default move otherwise.
@@ -18,6 +19,23 @@ public class MyBot : IChessBot {
         foreach (Move m in board.GetLegalMoves())
             if (move.StartSquare == m.StartSquare && move.TargetSquare == m.TargetSquare) return move;
         return def(board);
+    }
+
+    // Moves that give the opponent the fewest options.
+    List<Move> mostForcingMoves(Board board) {
+        int maxMoves = 300;
+        var myMoves = new List<Move>();
+        foreach (Move m in board.GetLegalMoves()) {
+            board.MakeMove(m);
+            var opponentMovesAmount = board.GetLegalMoves().Length;
+            if (opponentMovesAmount <= maxMoves) {
+                if (opponentMovesAmount < maxMoves) myMoves.Clear();
+                maxMoves = opponentMovesAmount;
+                myMoves.Add(m);
+            }
+            board.UndoMove(m);
+        }
+        return myMoves;
     }
 
     public Move Think(Board board, Timer timer) {
